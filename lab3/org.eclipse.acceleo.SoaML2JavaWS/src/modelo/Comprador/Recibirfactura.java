159b2bfebdfef99f31466b1ec95eaed7423753b3
Santiago Freire Lopez <santiago.freire@fing.edu.uy>
2023-08-29 19:51:40 -0300

    avances tarea 3

---
+++ lab3/org.eclipse.acceleo.SoaML2JavaWS/src/modelo/Comprador/Recibirfactura.java
@@ -0,0 +1,10 @@
+package modelo.Comprador;
+
+/*import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;*/
+
+public class Recibirfactura {
+
+}
---
+++ lab3/org.eclipse.acceleo.SoaML2JavaWS/src/modelo/Entidaddecredito/Recibirordendepago.java
@@ -0,0 +1,10 @@
+package modelo.Entidaddecredito;
+
+/*import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;*/
+
+public class Recibirordendepago {
+
+}
