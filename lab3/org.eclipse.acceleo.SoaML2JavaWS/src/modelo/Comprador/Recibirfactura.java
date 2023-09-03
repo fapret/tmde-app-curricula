0342904f2cdf286382bdfa0abe675249307e5455
Santiago Freire Lopez <santiago.freire@fing.edu.uy>
2023-09-03 13:37:19 -0300

    avances

--- lab3/org.eclipse.acceleo.SoaML2JavaWS/src/modelo/Comprador/Recibirfactura.java
+++ lab3/org.eclipse.acceleo.SoaML2JavaWS/src/modelo/Comprador/Recibirfactura.java
@@ -1,10 +1,16 @@
 package modelo.Comprador;
-
-/*import javax.jws.WebMethod;
+import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
-import javax.jws.soap.SOAPBinding.Style;*/
+import javax.jws.soap.SOAPBinding.Style;
 
 public class Recibirfactura {
+} 
 
+@WebMethod
+@SOAPBinding(style = Style.RPC)
+public interface Recibirfactura {
+
+	@WebMethod
+	String RecibirfacturaOperationReceive(String RecibirfacturaParameterIn);
 }
