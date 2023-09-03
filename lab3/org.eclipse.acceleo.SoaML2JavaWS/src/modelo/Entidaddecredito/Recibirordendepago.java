9a0ce1d5109d048cf425e40b6d42bd0473a9d32d
Santiago Freire Lopez <santiago.freire@fing.edu.uy>
2023-09-03 13:37:19 -0300

    avances

--- lab3/org.eclipse.acceleo.SoaML2JavaWS/src/modelo/Entidaddecredito/Recibirordendepago.java
+++ lab3/org.eclipse.acceleo.SoaML2JavaWS/src/modelo/Entidaddecredito/Recibirordendepago.java
@@ -1,10 +1,16 @@
 package modelo.Entidaddecredito;
-
-/*import javax.jws.WebMethod;
+import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
-import javax.jws.soap.SOAPBinding.Style;*/
+import javax.jws.soap.SOAPBinding.Style;
 
 public class Recibirordendepago {
+} 
 
+@WebMethod
+@SOAPBinding(style = Style.RPC)
+public interface Recibirordendepago {
+
+	@WebMethod
+	String RecibirordendepagoOperationReceive(String RecibirordendepagoParameterIn);
 }
--- lab3/org.eclipse.acceleo.SoaML2JavaWS/src/modelo/Vendedor/Recibirpago.java
+++ lab3/org.eclipse.acceleo.SoaML2JavaWS/src/modelo/Vendedor/Recibirpago.java
@@ -1,10 +1,16 @@
 package modelo.Vendedor;
-
-/*import javax.jws.WebMethod;
+import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
-import javax.jws.soap.SOAPBinding.Style;*/
+import javax.jws.soap.SOAPBinding.Style;
 
 public class Recibirpago {
+} 
 
+@WebMethod
+@SOAPBinding(style = Style.RPC)
+public interface Recibirpago {
+
+	@WebMethod
+	String RecibirpagoOperationReceive(String RecibirpagoParameterIn);
 }
