c44d4eeaa35ffa475689f5aa1bc5741f339170da
Fapret <78763775+fapret@users.noreply.github.com>
2023-11-07 20:26:01 -0300

    front test

---
+++ proy/front-final/js/getFaculties.js
@@ -0,0 +1,24 @@
+function consultarFacultad() {
+const allFacultiesSelect = document.getElementById("facultades");
+const facultyName = allFacultiesSelect.selectedOption;
+const url = `http://localhost:8080/curricula_microservice/Faculty?faculty=${facultyName}`;
+fetch(url)
+.then(response => response.json())
+.then(data => {
+    const resultadoDiv = document.getElementById("resultado");
+    resultadoDiv.innerHTML = "";
+
+    const name = data.Name;
+    const careers = data.Careers;
+    const facultyCU = data.FacultyCU;
+
+    resultadoDiv.innerHTML += `<p><strong>Faculty name:</strong> ${name}</p>`;
+    resultadoDiv.innerHTML += `<p><strong>Careers:</strong> ${careers.join(", ")}</p>`;
+    resultadoDiv.innerHTML += `<p><strong>Curricular Units:</strong> ${facultyCU.join(", ")}</p>`;
+})
+.catch(error => {
+    console.error("Error al consultar la API:", error);
+    const resultadoDiv = document.getElementById("resultado");
+    resultadoDiv.innerHTML = "Error al consultar la API: "+error;
+});
+}
