eb7169d42e9c1f545ea8b2aa89dbe87bd0cb78d3
Santiago Freire Lopez <santiago.freire@fing.edu.uy>
2023-11-12 22:56:07 -0300

    corregidos requirements en consulta uc

--- proy/front-final/js/getUC.js
+++ proy/front-final/js/getUC.js
@@ -1,3 +1,25 @@
+function printRequirements(data) {
+    let res = "";
+        for (const clave in data) {
+            // Verifica si la propiedad realmente pertenece al objeto (y no a su cadena de prototipo)
+            if (data.hasOwnProperty(clave)) {
+                // Imprime la clave y su valor
+                console.log(data[clave]);
+                if (clave === "Exam" || clave === "Coursed")
+                res += clave+": "+data[clave]+", ";
+                if (clave === "SomeOf") {
+                    res += "Some Of "+data[clave][0]+": ["
+                    for (let j = 1; j < data[clave].length; j++) {
+                        res += printRequirements(data[clave][j]);
+                    }
+                    res = res.substring(0,res.length-2);
+                    res += "], ";
+            }
+        }
+    }
+    return res;
+}
+
 function consultarUC() {
 const allFacultiesSelect = document.getElementById("facultades");
 const facultyName = allFacultiesSelect.value;
@@ -7,6 +29,10 @@ const url = `http://localhost:8080/curricula_microservice/Faculty/ucs?faculty=${
 fetch(url)
 .then(response => response.json())
 .then(data => {
+    let reqs = "";
+    for (let i = 0; i < data.Requirement.length; i++)
+        reqs += printRequirements(data.Requirement[i]);
+    reqs = reqs.substring(0,reqs.length-2);
     const resultadoDiv = document.getElementById("resultado");
     resultadoDiv.style.display = "block";
     resultadoDiv.innerHTML = "";
@@ -14,7 +40,7 @@ fetch(url)
             resultadoDiv.innerHTML += `<p><strong>Curricular Unit Name:</strong> ${data.Name}</p>`;
             resultadoDiv.innerHTML += `<p><strong>Curricular Unit ID:</strong> ${data.Id}</p>`;
             resultadoDiv.innerHTML += `<p><strong>Credits:</strong> ${data.Cred}</p>`;
-            resultadoDiv.innerHTML += `<p><strong>Requirements:</strong> ${data.Requirement || 'N/A'}</p>`;
+            resultadoDiv.innerHTML += `<p><strong>Requirements:</strong> ${reqs || 'N/A'}</p>`;
             resultadoDiv.innerHTML += `<p><strong>Exam evaluations:</strong> ${data.ExamEvaluation.join(', ')}</p>`;
 
             resultadoDiv.innerHTML += "<p><strong>Courses:</strong></p>";
