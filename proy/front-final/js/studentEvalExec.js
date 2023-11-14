091793dd0f9970151f9e377ef5bb89f299344eaf
Santiago Freire Lopez <santiago.freire@fing.edu.uy>
2023-11-14 01:05:32 -0300

    evaluar unidad curricular listo

---
+++ proy/front-final/js/studentEvalExec.js
@@ -0,0 +1,16 @@
+const allFacultiesSelect = document.getElementById("facultades");
+const allUCSelect = document.getElementById("ucs");
+const selectFacultyBtn = document.getElementById("markFaculty");
+const consultBtn = document.getElementById("markUC");
+consultBtn.disabled = true;
+
+getFaculties(allFacultiesSelect);
+selectFacultyBtn.addEventListener("click", () => {
+    getUCS(allUCSelect, allFacultiesSelect.value);
+    consultBtn.disabled = false;
+});
+consultBtn.addEventListener("click", () => {
+    document.getElementById("resultado").style.display = "flex";
+    document.getElementById("resultado").scrollIntoView({behavior: "smooth"});
+    evaluar_uc();
+});
