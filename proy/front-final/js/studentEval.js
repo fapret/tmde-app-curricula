e601c71d137979e7aceddc3b15f7fff8a1e98495
Santiago Freire Lopez <santiago.freire@fing.edu.uy>
2023-11-14 01:05:32 -0300

    evaluar unidad curricular listo

---
+++ proy/front-final/js/studentEval.js
@@ -0,0 +1,28 @@
+function evaluar_uc() {
+    const allFacultiesSelect = document.getElementById("facultades");
+    const allUCSelect = document.getElementById("ucs");
+    const facultyName = allFacultiesSelect.value;
+    const uc_id = allUCSelect.value;
+    const model_file = document.getElementById("file").files[0];
+
+    const url = `http://localhost:8080/curricula_microservice/Faculty/ucs/eval`;
+    var formData = new FormData();
+    formData.append('faculty', facultyName);
+    formData.append('curricularUnit', uc_id);
+    formData.append('file', model_file);
+
+    // Configurar las opciones de la solicitud
+    var options = {
+        method: 'POST',
+        body: formData
+    };
+
+    fetch(url, options)
+        .then(response => response.json())
+        .then(data => {
+            if (data)
+                document.getElementById("resultado").innerHTML = "Está habilitado para cursar la materia."
+            else
+                document.getElementById("resultado").innerHTML = "No está habilitado para cursar la materia."
+        })
+}
